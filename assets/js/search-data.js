09553854ddbaf5a43b39e15c9f199322b8ca23df
dunlopWill <62902332+dunlopWill@users.noreply.github.com>
2025-08-27 07:38:02 +0000

    Deploying to gh-pages from @ dunlopWill/dunlopWill.github.io@2126c98c9ca167e815470b3a07536a305a764f47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,14 +23,21 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-repos",
-          title: "repos",
-          description: "Explore selected repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repos/";
-          },
-        },{id: "post-developer-accountants",
+        },{id: "dropdown-web",
+              title: "web",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/web/";
+              },
+            },{id: "dropdown-repos",
+              title: "repos",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/repos/";
+              },
+            },{id: "post-developer-accountants",
         
           title: "developer accountants",
         
@@ -63,65 +70,10 @@ ninja.data = [{
             window.open("https://engine-shed.co.uk/news/reflections-from-our-scale-up-business-intern-will-dunlop/", "_blank");
           
         },
-      },{id: "books-entangled-life",
-          title: 'Entangled Life',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/entangled_life/";
-            },},{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-now-a-parent",
+      },{id: "news-now-a-parent",
           title: 'now a parent',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-excel-add-in",
+          section: "News",},{id: "projects-excel-add-in",
           title: 'Excel Add-in',
           description: "",
           section: "Projects",handler: () => {
